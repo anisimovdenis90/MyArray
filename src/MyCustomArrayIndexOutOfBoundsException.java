be6af561943a05d2ab94476c95bcd51708e4b19b
Denis Anisimov <anisimov.denis.90@gmail.com>
2020-10-29 00:01:27 +0300

    Практическая работа

---
+++ src/MyCustomArrayIndexOutOfBoundsException.java
@@ -0,0 +1,5 @@
+public class MyCustomArrayIndexOutOfBoundsException extends RuntimeException {
+    public MyCustomArrayIndexOutOfBoundsException(int index, int size) {
+        super(String.format("Invalid index %d for array with length %d", index, size));
+    }
+}
